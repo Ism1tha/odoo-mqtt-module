1176bf12d881401d94cbadbb54b4ba9b17f5c424
Ism1tha <xgalan@hotmail.com>
2025-02-20 12:24:24 +0100

    feat: update MQTT module manifest and configuration

--- addons/mqtt-module/__manifest__.py
+++ addons/mqtt-module/__manifest__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Odoo MQTT Module",
-    "description": """
-        A module to integrate MQTT communication with Odoo for robotic automation.
-    """,
+    "description": "A module to integrate MQTT communication with Odoo for robotic automation.",
     "author": "Ismael Semmar Galvez",
     "website": "https://github.com/Ism1tha/odoo-mqtt-module.git",
-    "category": "Automation/Communication",
+    "category": "Extra Tools",
     "version": "17.0.1.0.0",
-    "depends": [],
+    "license": "GPL-3",
+    "depends": ["base", "web"],
     "data": [],
-    "application": False,
+    "application": True,
 }
