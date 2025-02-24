f7afb3660d06e1f3e8a09d780c2bfb3c05338539
Ism1tha <xgalan@hotmail.com>
2025-02-24 21:47:30 +0100

    feat: add .gitignore, update MQTT module manifest, and include icon

--- addons/mqtt-module/__manifest__.py
+++ addons/mqtt-module/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Odoo MQTT Module",
+    "summary": "Integrate MQTT communication with Odoo for robotic automation.",
     "description": "A module to integrate MQTT communication with Odoo for robotic automation.",
     "author": "Ismael Semmar Galvez",
     "website": "https://github.com/Ism1tha/odoo-mqtt-module.git",
-    "category": "Extra Tools",
-    "version": "17.0.1.0.0",
+    "category": "Manufacturing",
+    "version": "1.0",
     "license": "GPL-3",
-    "depends": ["base", "web"],
-    "data": [],
+    "depends": ["mrp"],
     "application": True,
 }
